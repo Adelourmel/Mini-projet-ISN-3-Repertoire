79cb1bbf2c9277406836aa856c41af5cfc4fc281
Unshaft <33205504+Unshaft@users.noreply.github.com>
2018-01-15 17:30:00 +0100

    Update repertoire.py

--- repertoire.py
+++ repertoire.py
@@ -4,9 +4,12 @@ def recherche():
 	pass	
 
 def creationNouveauContact():
-	newcontact=input("Nom du contact:")
-	
-
+	nom=input("Nom du contact: ")
+	prenom=input("Prenom du contact: ")
+	numero=input("Numero du contact: ")
+	fichier=open("repertoire.txt","w")
+	fichier.write(nom,prenom,numero)
+	fichier.close
 def afficher():
 	pass
 

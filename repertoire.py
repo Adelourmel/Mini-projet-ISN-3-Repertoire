f72428fc75ded061afcace8cc91863e621a6e2c5
Arnaud Delourmel <a.delourmel35@gmail.com>
2018-02-14 18:59:41 +0100

    V1.3

--- repertoire.py
+++ repertoire.py
@@ -3,24 +3,25 @@ import io
 import collections
 from tkinter import *
 
-def recherche(fenetre, bouton1, bouton3, bouton2):
-	bouton1.grid_forget()
-	bouton2.grid_forget()
-	bouton3.grid_forget()
+def recherche(frame):
+	frame.forget()
 	
-	rechercheNumero = Button(fenetre, text="Rechercher par numero", command=lambda : numero(rechercheNumero, rechercheNom, recherchePrenom), bg="grey", activebackground="grey",font="arial", height="2")
+	frameRecherche = Frame(fenetre, width=550, height=250)
+	frameRecherche.pack(fill=BOTH)
+	rechercheNumero = Button(frameRecherche, text="Rechercher par numero", command=lambda : numero(frameRecherche), bg="grey", activebackground="grey",font="arial", height="2")
 	rechercheNumero.grid(row=1,column=5)
 	
-	rechercheNom = Button(fenetre, text="Rechercher par nom", command=lambda : enregistrement(rechercheNumero, rechercheNom, recherchePrenom), bg="grey", activebackground="grey",font="arial", height="2")
+	rechercheNom = Button(frameRecherche, text="Rechercher par nom", command=lambda : enregistrement(frameRecherche), bg="grey", activebackground="grey",font="arial", height="2")
 	rechercheNom.grid(row=2,column=5)
 	
-	recherchePrenom = Button(fenetre, text="Rechercher par prenom", command=lambda : enregistrement(rechercheNumero, rechercheNom, recherchePrenom), bg="grey", activebackground="grey",font="arial", height="2")
+	recherchePrenom = Button(frameRecherche, text="Rechercher par prenom", command=lambda : enregistrement(frameRecherche), bg="grey", activebackground="grey",font="arial", height="2")
 	recherchePrenom.grid(row=3,column=5)
 
-def numero(entr1, entr2, entr3):
-	entr1.grid_forget()
-	entr2.grid_forget()
-	entr3.grid_forget()
+	boutonRetour = Button(frameRecherche, text="Retour au Menu", command=lambda : menu(frameRecherche), bg="grey", activebackground="grey", font="arial", height="2")
+	boutonRetour.grid(row = 4, column=5)
+
+def numero(frame):
+
 	dictionnaire = {}
 	fichier = open("repertoire.txt", "r")
 	fichier.seek(0)
@@ -29,20 +30,21 @@ def numero(entr1, entr2, entr3):
 		prenom = fichier.readline()
 		numero = fichier.readline()
 		dictionnaire[numero] = (line, prenom)
-
-	labelNumero = Label(fenetre, text='Numéro de téléphone:',fg="white", bg="grey" )
+	frame.forget()
+	frameNumero = Frame(fenetre, width=550, height=250)
+	frameNumero.pack(fill=BOTH)
+	labelNumero = Label(frameNumero, text='Numéro de téléphone:',fg="white", bg="grey" )
 	labelNumero.grid(row=2,column=3, sticky=W)
 	varNumero = StringVar()
-	entrNumero = Entry(fenetre, textvariable=varNumero, width=30)
+	entrNumero = Entry(frameNumero, textvariable=varNumero, width=30)
 	entrNumero.grid(row=2,column=4, sticky=W)
-	labelNom = Label(fenetre, text=dictionnaire.get(entrNumero),fg="white", bg="grey" )
+	labelNom = Label(frameNumero, text=dictionnaire.get(entrNumero),fg="white", bg="grey" )
 	labelNumero.grid(row=2,column=3, sticky=W)
 
-	
-
+	boutonRetour = Button(frameNumero, text="Retour au Menu", command=lambda : menu(frameNumero), bg="grey", activebackground="grey", font="arial", height="2")
+	boutonRetour.grid(row = 4, column=4)
 
 def enregistrement(entr1, entr2, entr3):
-	print("click")
 	nom = entr2.get()
 	print(nom)
 	prenom = entr1.get()
@@ -54,38 +56,36 @@ def enregistrement(entr1, entr2, entr3):
 	entr2.delete(0, len(nom))
 	entr3.delete(0, len(numero))
 
-def creationNouveauContact(fenetre, bouton1, bouton3, bouton2):
-
-	bouton1.grid_forget()
-	bouton2.grid_forget()
-	bouton3.grid_forget()
+def creationNouveauContact(frame):
+	frame.forget()
 	
-	labelPrenom = Label(fenetre, text='Prenom:',fg="white", bg="grey" )
+	frameCreationNouveauContact = Frame(fenetre, width=550, height=250)
+	frameCreationNouveauContact.pack(fill=BOTH)
+	labelPrenom = Label(frameCreationNouveauContact, text='Prenom:',fg="white", bg="grey" )
 	labelPrenom.grid(row=0,column=3, sticky=W)
 	varPrenom = StringVar()
-	entrPrenom = Entry(fenetre, textvariable=varPrenom, width=30)
+	entrPrenom = Entry(frameCreationNouveauContact, textvariable=varPrenom, width=30)
 	entrPrenom.grid(row=0,column=4, sticky=W)
 
-	labelNom = Label(fenetre, text='Nom:',fg="white", bg="grey" )
+	labelNom = Label(frameCreationNouveauContact, text='Nom:',fg="white", bg="grey" )
 	labelNom.grid(row=1,column=3, sticky=W)
 	varNom = StringVar()
-	entrNom = Entry(fenetre, textvariable=varNom, width=30)
+	entrNom = Entry(frameCreationNouveauContact, textvariable=varNom, width=30)
 	entrNom.grid(row=1,column=4, sticky=W)
 
-	labelNumero = Label(fenetre, text='Numéro de téléphone:',fg="white", bg="grey" )
+	labelNumero = Label(frameCreationNouveauContact, text='Numéro de téléphone:',fg="white", bg="grey" )
 	labelNumero.grid(row=2,column=3, sticky=W)
 	varNumero = StringVar()
-	entrNumero = Entry(fenetre, textvariable=varNumero, width=30)
+	entrNumero = Entry(frameCreationNouveauContact, textvariable=varNumero, width=30)
 	entrNumero.grid(row=2,column=4, sticky=W)
 
-
-	boutonAjouter = Button(fenetre, text="Ajouter", command=lambda : enregistrement(entrPrenom, entrNom, entrNumero), bg="grey", activebackground="grey",font="arial", height="2")
+	boutonAjouter = Button(frameCreationNouveauContact, text="Ajouter", command=lambda : enregistrement(entrPrenom, entrNom, entrNumero), bg="grey", activebackground="grey",font="arial", height="2")
 	boutonAjouter.grid(row=3,column=4)
-	boutonRetour = Button(fenetre, text="Retour", command=lambda : menu(option=1, bouton1=boutonRetour, bouton2=boutonAjouter, entr1=entrPrenom, entr2=entrNumero, entr3=entrNom, label1=labelPrenom, label2=labelNumero, label3=labelNom), bg="grey", activebackground="grey", font="arial", height="2")
+	boutonRetour = Button(frameCreationNouveauContact, text="Retour au Menu", command=lambda : menu(frameCreationNouveauContact), bg="grey", activebackground="grey", font="arial", height="2")
 	boutonRetour.grid(row = 4, column=4)
 
 
-def afficher(fenetre, bouton2, bouton3, bouton1):
+def afficher(frame):
 	dictionnaire = {}
 	fichier = open("repertoire.txt", "r")
 	fichier.seek(0)
@@ -97,43 +97,29 @@ def afficher(fenetre, bouton2, bouton3, bouton1):
 	bouton1.grid_forget()
 	bouton2.grid_forget()
 	bouton3.grid_forget()
-	bouton1 = Button(fenetre, text="Retour", command=lambda : menu(option=2), bg="grey", activebackground="grey", font="arial", height="2")
+	bouton1 = Button(fenetre, text="Retour au Menu", command=lambda : menu(option=2), bg="grey", activebackground="grey", font="arial", height="2")
 	bouton1.grid(row = 4, column=4)
 	for ligne in range(5):
 	    for colonne in range(5):
     		Button(fenetre, text='L%s-C%s' % (ligne, colonne), borderwidth=1).grid(row=ligne, column=colonne)
 
 
-def menu(option=0, bouton1=1, bouton2= 1, bouton3= 1, entr1=1, entr2=1, entr3=1, label1=1, label2=1, label3=1):
-	if option == 0:
-		pass
-	elif option == 1:
-		bouton1.grid_forget()
-		bouton2.grid_forget()
-		entr1.grid_forget()
-		entr2.grid_forget()
-		entr3.grid_forget()
-		label1.grid_forget()
-		label2.grid_forget()
-		label3.grid_forget()
+def menu(frame):
+	if frame != 1:
+		frame.forget()
 
-	else:
-		pass
-
-	
-	label = Label(fenetre, text="accueil")
-	boutonAfficher = Button(fenetre, text="Afficher les contacts", command=lambda : afficher(fenetre, boutonNvContact, boutonAfficher, boutonRecherche), bg="grey", activebackground="grey",font="arial", height="2")
+	menu = Frame(fenetre, width=550, height=250)
+	menu.pack(fill=BOTH)
+	boutonAfficher = Button(menu, text="Afficher les contacts", command=lambda : afficher(menu), bg="grey", activebackground="grey",font="arial", height="2")
 	boutonAfficher.grid(row=1,column=5)
-	boutonRecherche = Button(fenetre, text="Rechercher un contact", command=lambda : recherche(fenetre, boutonNvContact, boutonAfficher, boutonRecherche), bg="grey", activebackground="grey",font="arial", height="2")
+	boutonRecherche = Button(menu, text="Rechercher un contact", command=lambda : recherche(menu), bg="grey", activebackground="grey",font="arial", height="2")
 	boutonRecherche.grid(row=2,column=5)
-	boutonNvContact = Button(fenetre, text="Ajouter un nouveau contact", command=lambda : creationNouveauContact(fenetre, boutonNvContact, boutonAfficher, boutonRecherche), bg="grey", activebackground="grey",font="arial", height="2")
+	boutonNvContact = Button(menu, text="Ajouter un nouveau contact", command=lambda : creationNouveauContact(menu), bg="grey", activebackground="grey",font="arial", height="2")
 	boutonNvContact.grid(row=3,column=5)
 	fenetre.mainloop()
-
-
 
 
 fenetre = Tk()
 fenetre.geometry('550x250')
 fenetre.title("Repertoire")
-menu()
+menu(1)
